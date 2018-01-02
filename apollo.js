aad2f61bef4c6478a82b0a7bfafbcce55ab2e8ef
HiImMardzin <marcin4951@gmail.com>
2018-01-02 21:09:37 +0100

    Fixing apollo

--- apollo.js
+++ apollo.js
@@ -1,38 +1,20 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
-import { split } from 'apollo-link'
 import { HttpLink } from 'apollo-link-http'
-import { WebSocketLink } from 'apollo-link-ws'
-import { getMainDefinition } from 'apollo-utilities'
-// import { setContext } from 'apollo-link-context'
+import { setContext } from 'apollo-link-context'
 
 const httpLink = new HttpLink({ uri: 'http://192.168.0.106:4000/graphql' })
 
-// const middlewareLink = setContext(async () => ({
-//     headers: {
-//       'x-token': await AsyncStorage.getItem('token')
-//     },
-// }))
-
-const wsLink = new WebSocketLink({
-  uri: `ws://192.168.0.106:4000/subscriptions`,
-  options: {
-    reconnect: true
-  }
+const authLink = setContext((_, { headers }) => {
+    return {
+      headers: {
+        ...headers,
+        'x-token': 'token'
+      }
+    }
 })
 
-// const linkConcat = middlewareLink.concat(httpLink)
-
-const link = split(
-  ({ query }) => {
-    const { kind, operation } = getMainDefinition(query)
-    return kind === 'OperationDefinition' && operation === 'subscription'
-  }, 
-  wsLink,
-  httpLink,
-)
-
 export const client = new ApolloClient({
-    link,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
 })
